ace4d4fcf24a597b91b36ab18ea4dc030bd2ba0e
Tharinda Pamindu <tharindapamindu@gmail.com>
2026-03-11 16:53:11 +0530

    init register and login activities

--- app/src/main/java/com/example/vocably/auth/Login.java
+++ app/src/main/java/com/example/vocably/auth/Login.java
@@ -74,25 +74,6 @@ public class Login extends AppCompatActivity {
 
         btnLogin.setOnClickListener(V -> {
             //TODO : Implement login functionality
-
-            String email = txtEmail.getEditText().getText().toString();
-            String password = txtPassword.getEditText().getText().toString();
-
-            mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d(TAG, "signInWithEmail:success");
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w(TAG, "signInWithEmail:failure", task.getException());
-                                Toast.makeText(Login.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
         });
 
     }
